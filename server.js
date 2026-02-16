a55982cf35aeffc6331e3c0d041e939288358f6c
nikhilpatidar7077 <nikhilpatidar7077@gmail.com>
2026-02-16 19:32:20 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+const app = express();
+dotenv.config();
+connectDB();
+
+app.get("/",(req,res)=>{
+    res.send("CareerForge Pro in running")
+});
+
+app.listen(process.env.PORT,()=>{
+console.log(`server running on port ${process.env.PORT}`)
+});
